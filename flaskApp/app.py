1ca3244aa2fa323887a3b77c4cf5109ca1d47b5b
Krishanus-2001 <krishanu21saini@gmail.com>
2022-03-25 01:18:50 +0530

    Add website

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,37 +1,74 @@
-from flask import Flask, render_template, request
-import imageio as iio
+from operator import index
+from flask import Flask, render_template, request, redirect, flash, url_for, jsonify
 import numpy as np
-import keras.models
+from keras.models import load_model
 import cv2
 import re
-import sys 
-import os
+import requests
+from werkzeug.utils import secure_filename
 import base64
 from load import * 
+import os
 
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 global graph, model
 
 model, graph = init()
 
-app = Flask(__name__)
+# app name
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
-def index_view():
+@app.route('/home', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('learn', name=filename))
     return render_template('index.html')
 
+@app.route('/learn', methods=['GET'])
+def learn():
+    name = request.args.get('name')
+    res = requests.get('http://localhost:8000/googlenet', params = {'name': name} )
+    x = (res.json()['response'])
+    res = requests.get('http://localhost:8000/vgg', params = {'name': name} )
+    y = (res.json()['response'])
+    return render_template('learn.html', name = name, googlenet = x, vgg = y)
+    
 def convertImage(imgData1):
 	imgstr = re.search(b'base64,(.*)',imgData1).group(1)
 	with open('output.png','wb') as output:
 	    output.write(base64.b64decode(imgstr))
 
-@app.route('/predict/',methods=['GET','POST'])
-def predict():
+@app.route('/googlenet/',methods=['GET','POST'])
+def googlenet():
   # imgData = request.get_data()
   # convertImage(imgData)
-  
-  x = cv2.imread('images/airplane1.png', cv2.IMREAD_GRAYSCALE)
+  name = request.args.get('name')
+  x = cv2.imread('uploads/' + name, cv2.IMREAD_GRAYSCALE)
   # print(x)
   # x = np.invert(x)
   res = cv2.resize(x, dsize=(32, 32), interpolation=cv2.INTER_CUBIC)
@@ -44,16 +81,45 @@ def predict():
         loaded_model = model_from_json(loaded_model_json)
         #load weights into new model
         loaded_model.load_weights("model/model.h5")
-        print("Loaded Model from disk")
         #compile and evaluate loaded model
         loaded_model.compile(loss=['categorical_crossentropy', 'categorical_crossentropy', 'categorical_crossentropy'] ,optimizer='adam',metrics=['accuracy'])
         # perform the prediction
         out = loaded_model.predict(res)
         out = np.array(out[0])
-        print(out.shape)
         # convert the response to a string
         response = class_names[np.argmax(out, axis=1)[0]]
-        return str(response)
+        return jsonify(
+            response=response,
+        )
+
+# scale pixels
+def prep_pixels(train):
+	# convert from integers to floats
+	train_norm = train.astype('float32')
+	# normalize to range 0-1
+	train_norm = train_norm / 255.0
+	# return normalized images
+	return train_norm
+
+@app.route('/vgg/', methods=['GET', 'POST'])
+def vgg():
+    name = request.args.get('name')
+    x = cv2.imread('uploads/' + name)
+    # print(x)
+    # x = np.invert(x)
+    res = cv2.resize(x, dsize=(32, 32), interpolation=cv2.INTER_CUBIC)
+    class_names = ["airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"]
+    # load model
+    model = load_model('model/VGG16_model.h5')
+    trainX = prep_pixels(res)
+    trainX = np.array(trainX).reshape(1, 32, 32, 3)
+    out = model.predict(trainX)
+    out = np.array(out)
+    # convert the response to a string
+    response = class_names[np.argmax(out, axis=1)[0]]
+    return jsonify(
+        response=response,
+    )
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host='localhost', port=8000)
